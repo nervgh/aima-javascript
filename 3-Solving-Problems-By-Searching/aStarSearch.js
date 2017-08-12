1641982479997df49d560f355c9478113d39995c
nerv <nervgh@gmail.com>
2017-08-12 14:45:10 +0300

    aStarSearch algorithm (part 1)

--- 3-Solving-Problems-By-Searching/aStarSearch.js
+++ 3-Solving-Problems-By-Searching/aStarSearch.js
@@ -1,1 +1,213 @@
+
+// Standard Code Style -- https://github.com/standard/standard
+// ESLint Config -- http://eslint.org/docs/user-guide/configuring
+
+/* eslint-env browser */
+/* global DefaultGraph */
+/* global GraphProblem */
+/* global GraphAgent */
+
+// The default graph that is used in most of the simulation
+// This ensures consistency in the experience of the user
+class GraphAStarSearch extends DefaultGraph {
+  constructor () {
+    super()
+    // It adapts the default graph data for using h(n) heuristic
+    // this.nodes['J'].y -= 25
+    GraphAStarSearch.reWeightEdges(this)
+  }
+  static reWeightEdges (graph) {
+    for (let edge of graph.edges) {
+      const nodeKeyA = edge[0]
+      const nodeKeyB = edge[1]
+      const nodeA = graph.nodes[nodeKeyA]
+      const nodeB = graph.nodes[nodeKeyB]
+      const pointA = [nodeA.x, nodeA.y]
+      const pointB = [nodeB.x, nodeB.y]
+      const distance = GraphProblemAStarSearch.euclideanDistance(pointA, pointB)
+      const cost = Math.round(distance)
+      // console.log('%s -> %s, distance=%s, cost=%s', nodeKeyA, nodeKeyB, distance, cost)
+      edge[2] = cost
+    }
+  }
+}
+
 // Code for A Star Search
+
+// TODO: Should we use ES5 syntax for support old browsers?
+class GraphProblemAStarSearch extends GraphProblem {
+  /**
+   * @see https://github.com/nervgh/astar-algorithm
+   * @param {Object} nodes
+   * @param {Object} edges
+   * @param {String} initialKey
+   * @param {String} nextToExpand
+   * @param {String} goalKey
+   */
+  constructor (nodes, edges, initialKey, nextToExpand, goalKey) {
+    super(nodes, edges, initialKey, nextToExpand)
+    this.goalKey = goalKey
+  }
+  /**
+   * It should check: is a node is the goal?
+   * @param {String} nodeKey
+   * @return {Boolean}
+   */
+  isGoal (nodeKey) {
+    return this.goalKey === nodeKey
+  }
+  /**
+   * g(x). It should return the cost of path between two nodes
+   * @param {String} nodeKeyA
+   * @param {String} nodeKeyB
+   * @return {Number}
+   */
+  distance (nodeKeyA, nodeKeyB) {
+    for (const [keyA, keyB, cost] of this.edges) {
+      if (GraphProblemAStarSearch.isEqualNodeKeyPair(nodeKeyA, nodeKeyB, keyA, keyB)) {
+        return cost
+      }
+    }
+    return Number.POSITIVE_INFINITY
+  }
+  /**
+   * h(x). It should return the cost of path from a node to the goal
+   * @param {String} nodeKey
+   * @param {String} [goalKey]
+   * @return {Number}
+   */
+  estimate (nodeKey, goalKey = this.goalKey) {
+    const nodeA = this.nodes[nodeKey]
+    const nodeB = this.nodes[goalKey]
+    const point1 = [nodeA.x, nodeA.y]
+    const point2 = [nodeB.x, nodeB.y]
+    const estimated = GraphProblemAStarSearch.euclideanDistance(point1, point2)
+    return Math.round(estimated)
+  }
+  /**
+   * @param {String} nodeKey
+   * @return {Array.<GraphNode>}
+   */
+  getSuccessors (nodeKey) {
+    return this.getAdjacent(nodeKey).map(item => this.nodes[item.nodeKey])
+  }
+  /**
+   * Resets problem
+   */
+  reset () {
+    super.reset()
+    for (const node of GraphProblemAStarSearch.toArray(this.nodes)) {
+      node.totalCost = 0
+    }
+  }
+  /**
+   * @return {Boolean}
+   */
+  inProgress () {
+    return this.explored.length > 0 && !this.isSolved()
+  }
+  /**
+   * @return {Boolean}
+   */
+  isSolved () {
+    return this.isGoal(this.frontier[0])
+  }
+  /**
+   * @param {String} nodeKey
+   * @return {Boolean}
+   */
+  isQueuedNode (nodeKey) {
+    return this.frontier.indexOf(nodeKey) !== -1
+  }
+  /**
+   * @param {String} nodeKey
+   * @return {Boolean}
+   */
+  isExploredNode (nodeKey) {
+    return this.explored.indexOf(nodeKey) !== -1
+  }
+  /**
+   * @param {String} nodeKeyA1
+   * @param {String} nodeKeyB1
+   * @param {String} nodeKeyA2
+   * @param {String} nodeKeyB2
+   * @return {Boolean}
+   */
+  static isEqualNodeKeyPair (nodeKeyA1, nodeKeyB1, nodeKeyA2, nodeKeyB2) {
+    return (nodeKeyA1 === nodeKeyA2 && nodeKeyB1 === nodeKeyB2) ||
+           (nodeKeyA1 === nodeKeyB2 && nodeKeyB1 === nodeKeyA2)
+  }
+  /**
+   * @param {Array.<Number>} point1
+   * @param {Array.<Number>} point2
+   * @return {Number}
+   */
+  static euclideanDistance (point1, point2) {
+    return Math.sqrt(Math.pow(point1[0] - point2[0], 2) + Math.pow(point1[1] - point2[1], 2))
+  }
+  /**
+   * Turns an object to an array of objects
+   * @param {Object} obj
+   * @return {Array.<Object>}
+   */
+  static toArray (obj) {
+    let stack = []
+    for (let key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        stack.push(obj[key])
+      }
+    }
+    return stack
+  }
+}
+
+// TODO: Should we use ES5 syntax for support old browsers?
+class GraphAgentAStarSearch extends GraphAgent {
+  /**
+   * The function that expands a node from the graph
+   * @see https://github.com/nervgh/astar-algorithm
+   * @param {String} nodeKey
+   */
+  expand (nodeKey) {
+    const parentNode = this.problem.nodes[nodeKey]
+
+    if (this.problem.isGoal(parentNode.id)) {
+      // TODO: parentNode.state = 'goal'
+      return
+    }
+
+    this.problem.removeFromFrontier(parentNode.id)
+    this.problem.addToExplored(parentNode.id)
+
+    for (const successorNode of this.problem.getSuccessors(parentNode.id)) {
+      if (this.problem.isExploredNode(successorNode.id)) {
+        continue
+      }
+
+      successorNode.depth = parentNode.depth + 1
+      successorNode.parent = parentNode.id
+
+      // The distance from start to a successor
+      const tentativeGScore = parentNode.cost + this.problem.distance(parentNode.id, successorNode.id)
+
+      // This is not a better path
+      if (tentativeGScore >= successorNode.cost) {
+        continue
+      }
+
+      // This path is the best until now. We should save it.
+      successorNode.cost = tentativeGScore
+      successorNode.estimatedCost = this.problem.estimate(successorNode.id)
+      successorNode.totalCost = successorNode.cost + successorNode.estimatedCost
+
+      if (!this.problem.isQueuedNode(successorNode.id)) {
+        this.problem.addToFrontier(successorNode.id)
+      }
+    }
+
+    // We should prioritize the queue items
+    this.problem.frontier.sort((keyA, keyB) => {
+      return this.problem.nodes[keyA].totalCost - this.problem.nodes[keyB].totalCost
+    })
+  }
+}
--- 3-Solving-Problems-By-Searching/c_aStarSearch.js
+++ 3-Solving-Problems-By-Searching/c_aStarSearch.js
@@ -1,10 +1,178 @@
-$(document).ready(function() {
-  $.ajax({
-    url: "aStarSearch.js",
-    dataType: "text",
-    success: function(data) {
-      $("#aStarSearchCode").html(data);
-    }
-  });
+// Standard Code Style -- https://github.com/standard/standard
+// ESLint Config -- http://eslint.org/docs/user-guide/configuring
 
-});
+/* eslint-env browser */
+
+/* global Vue */
+/* global GraphAStarSearch */
+/* global GraphProblemAStarSearch */
+/* global GraphAgentAStarSearch */
+/* global DefaultOptions */
+/* global GraphDrawAgent */
+
+window.vmAStarSearch = new Vue({
+  /**
+   * The mount point
+   */
+  el: '#aStarSearchBox',
+  /**
+   * We should define our data here
+   * @return {Object}
+   */
+  data: function () {
+    // namespace
+    var aima = {}
+
+    // The default graph
+    aima.graph = new GraphAStarSearch()
+    aima.graphProblem = new GraphProblemAStarSearch(
+      aima.graph.nodes,
+      aima.graph.edges,
+      'A',
+      'A',
+      'O'
+    )
+
+    aima.options = new DefaultOptions()
+    aima.options.nodes.next.fill = 'hsla(126, 100%, 69%, 1)'
+    aima.options.edges.showCost = true
+
+    aima.delay = 2000 // ms
+    // It should color this node as "next" one
+    aima.graphProblem.nodes[aima.graphProblem.initialKey].state = 'next'
+
+    console.log('aStarSearch:aima', aima)
+
+    return {
+      aima: aima,
+      timerId: null
+    }
+  },
+  /**
+   * @see https://vuejs.org/v2/api/#mounted
+   */
+  mounted: function () {
+    var canvasOptions = {
+      width: 600,
+      height: 350
+    }
+
+    this.graphDrawAgent = new GraphDrawAgent(
+      this.aima.graphProblem,
+      'aStarSearchCanvas',
+      this.aima.options,
+      canvasOptions.height,
+      canvasOptions.width
+    )
+
+    this.graphAgent = new GraphAgentAStarSearch(
+      this.aima.graphProblem
+    )
+  },
+  methods: {
+    /**
+     * It plays, pauses the animation
+     */
+    playOrPause: function () {
+      if (this.timerId) {
+        this.timerId = clearTimeout(this.timerId)
+        return
+      }
+
+      if (this.aima.graphProblem.isSolved()) {
+        this.reset()
+      }
+      var callback = function () {
+        if (this.aima.graphProblem.isSolved()) {
+          this.timerId = clearTimeout(this.timerId)
+        } else {
+          this.next()
+          this.timerId = setTimeout(callback, this.aima.delay)
+        }
+      }.bind(this)
+
+      this.timerId = setTimeout(callback, this.aima.delay)
+    },
+    /**
+     * It renders next "frame" of visualization
+     */
+    next: function () {
+      if (this.aima.graphProblem.isSolved()) {
+        return
+      }
+
+      // Do next step
+      var nextNodeKey = this.aima.graphProblem.frontier[0]
+      this.graphAgent.expand(nextNodeKey)
+
+      // Do some things AFTER the step: We should colorize next node
+      var nextIterationNodeKey = this.aima.graphProblem.frontier[0]
+      var nextIterationNode = this.aima.graphProblem.nodes[nextIterationNodeKey]
+      nextIterationNode.state = 'next'
+
+      // Visualize the graph
+      this.graphDrawAgent.iterate()
+    },
+    /**
+     * It resets the visualization state
+     */
+    reset: function () {
+      this.aima.graphProblem.reset()
+      this.aima.graphProblem.nodes[this.aima.graphProblem.initialKey].state = 'next'
+      this.graphDrawAgent.iterate() // updates graph
+    },
+    /**
+     * @param {GraphProblem} graphProblem
+     * @return {Array.<Object>}
+     */
+    unexploredNodes: function (graphProblem) {
+      return GraphProblemAStarSearch.toArray(graphProblem.nodes).filter(function (node) {
+        return node.state === 'unexplored'
+      })
+    },
+    /**
+     * @param {GraphProblem} graphProblem
+     * @return {Array.<Object>}
+     */
+    frontierNodes: function (graphProblem) {
+      return graphProblem.frontier.map(function (nodeKey) {
+        return graphProblem.nodes[nodeKey]
+      })
+    },
+    /**
+     * @param {GraphProblem} graphProblem
+     * @return {Array.<Object>}
+     */
+    exploredNodes: function (graphProblem) {
+      return graphProblem.explored.map(function (nodeKey) {
+        return graphProblem.nodes[nodeKey]
+      })
+    },
+    /**
+     * @param {GraphProblem} graphProblem
+     * @return {Array.<Object>}
+     */
+    getPathNodes: function (graphProblem) {
+      var currentKey = graphProblem.frontier[0]
+      var node = graphProblem.nodes[currentKey]
+      var stack = []
+      while (node) {
+        stack.push(node)
+        var parentKey = node.parent
+        node = graphProblem.nodes[parentKey]
+      }
+      return stack.reverse()
+    },
+    /**
+     * @param {GraphProblem} graphProblem
+     * @param {DefaultOptions} options
+     * @param {GraphNode} node
+     * @return {{backgroundColor: string}}
+     */
+    getNodeStyle: function (graphProblem, options, node) {
+      return {
+        backgroundColor: options.nodes[node.state].fill
+      }
+    }
+  }
+})
