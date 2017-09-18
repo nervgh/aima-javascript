10147a3bb389f420d497ce70571ab724369c0fba
nerv <nervgh@gmail.com>
2017-09-18 16:32:22 +0300

    We should reformat code with prettier

--- 3-Solving-Problems-By-Searching/aStarSearch.js
+++ 3-Solving-Problems-By-Searching/aStarSearch.js
@@ -1,4 +1,3 @@
-
 // Standard Code Style -- https://github.com/standard/standard
 // ESLint Config -- http://eslint.org/docs/user-guide/configuring
 
@@ -11,24 +10,24 @@
 // The default graph that is used in most of the simulation
 // This ensures consistency in the experience of the user
 class GraphAStarSearch extends DefaultGraph {
-  constructor () {
-    super()
+  constructor() {
+    super();
     // It adapts the default graph data for using h(n) heuristic
     // this.nodes['J'].y -= 25
-    GraphAStarSearch.reWeightEdges(this)
+    GraphAStarSearch.reWeightEdges(this);
   }
-  static reWeightEdges (graph) {
+  static reWeightEdges(graph) {
     for (let edge of graph.edges) {
-      const nodeKeyA = edge[0]
-      const nodeKeyB = edge[1]
-      const nodeA = graph.nodes[nodeKeyA]
-      const nodeB = graph.nodes[nodeKeyB]
-      const pointA = [nodeA.x, nodeA.y]
-      const pointB = [nodeB.x, nodeB.y]
-      const distance = euclideanDistance(pointA, pointB)
-      const cost = Math.round(distance)
+      const nodeKeyA = edge[0];
+      const nodeKeyB = edge[1];
+      const nodeA = graph.nodes[nodeKeyA];
+      const nodeB = graph.nodes[nodeKeyB];
+      const pointA = [nodeA.x, nodeA.y];
+      const pointB = [nodeB.x, nodeB.y];
+      const distance = euclideanDistance(pointA, pointB);
+      const cost = Math.round(distance);
       // console.log('%s -> %s, distance=%s, cost=%s', nodeKeyA, nodeKeyB, distance, cost)
-      edge[2] = cost
+      edge[2] = cost;
     }
   }
 }
@@ -44,17 +43,17 @@ class GraphProblemAStarSearch extends GraphProblem {
    * @param {String} nextToExpand
    * @param {String} goalKey
    */
-  constructor (nodes, edges, initialKey, nextToExpand, goalKey) {
-    super(nodes, edges, initialKey, nextToExpand)
-    this.goalKey = goalKey
+  constructor(nodes, edges, initialKey, nextToExpand, goalKey) {
+    super(nodes, edges, initialKey, nextToExpand);
+    this.goalKey = goalKey;
   }
   /**
    * It should check: is a node is the goal?
    * @param {String} nodeKey
    * @return {Boolean}
    */
-  isGoal (nodeKey) {
-    return this.goalKey === nodeKey
+  isGoal(nodeKey) {
+    return this.goalKey === nodeKey;
   }
   /**
    * g(x). It should return the cost of path between two nodes
@@ -62,13 +61,20 @@ class GraphProblemAStarSearch extends GraphProblem {
    * @param {String} nodeKeyB
    * @return {Number}
    */
-  distance (nodeKeyA, nodeKeyB) {
+  distance(nodeKeyA, nodeKeyB) {
     for (const [keyA, keyB, cost] of this.edges) {
-      if (GraphProblemAStarSearch.isEqualNodeKeyPair(nodeKeyA, nodeKeyB, keyA, keyB)) {
-        return cost
+      if (
+        GraphProblemAStarSearch.isEqualNodeKeyPair(
+          nodeKeyA,
+          nodeKeyB,
+          keyA,
+          keyB
+        )
+      ) {
+        return cost;
       }
     }
-    return Number.POSITIVE_INFINITY
+    return Number.POSITIVE_INFINITY;
   }
   /**
    * h(x). It should return the cost of path from a node to the goal
@@ -76,43 +82,43 @@ class GraphProblemAStarSearch extends GraphProblem {
    * @param {String} [goalKey]
    * @return {Number}
    */
-  estimate (nodeKey, goalKey = this.goalKey) {
-    const nodeA = this.nodes[nodeKey]
-    const nodeB = this.nodes[goalKey]
-    const point1 = [nodeA.x, nodeA.y]
-    const point2 = [nodeB.x, nodeB.y]
-    const estimated = euclideanDistance(point1, point2)
-    return Math.round(estimated)
+  estimate(nodeKey, goalKey = this.goalKey) {
+    const nodeA = this.nodes[nodeKey];
+    const nodeB = this.nodes[goalKey];
+    const point1 = [nodeA.x, nodeA.y];
+    const point2 = [nodeB.x, nodeB.y];
+    const estimated = euclideanDistance(point1, point2);
+    return Math.round(estimated);
   }
   /**
    * @param {String} nodeKey
    * @return {Array.<GraphNode>}
    */
-  getSuccessors (nodeKey) {
-    return this.getAdjacent(nodeKey).map(item => this.nodes[item.nodeKey])
+  getSuccessors(nodeKey) {
+    return this.getAdjacent(nodeKey).map(item => this.nodes[item.nodeKey]);
   }
   /**
    * Resets problem
    */
-  reset () {
-    super.reset()
+  reset() {
+    super.reset();
     for (const node of GraphProblemAStarSearch.toArray(this.nodes)) {
-      node.totalCost = 0
+      node.totalCost = 0;
     }
   }
   /**
    * @param {String} nodeKey
    * @return {Boolean}
    */
-  isQueuedNode (nodeKey) {
-    return this.frontier.indexOf(nodeKey) !== -1
+  isQueuedNode(nodeKey) {
+    return this.frontier.indexOf(nodeKey) !== -1;
   }
   /**
    * @param {String} nodeKey
    * @return {Boolean}
    */
-  isExploredNode (nodeKey) {
-    return this.explored.indexOf(nodeKey) !== -1
+  isExploredNode(nodeKey) {
+    return this.explored.indexOf(nodeKey) !== -1;
   }
   /**
    * @param {String} nodeKeyA1
@@ -121,23 +127,25 @@ class GraphProblemAStarSearch extends GraphProblem {
    * @param {String} nodeKeyB2
    * @return {Boolean}
    */
-  static isEqualNodeKeyPair (nodeKeyA1, nodeKeyB1, nodeKeyA2, nodeKeyB2) {
-    return (nodeKeyA1 === nodeKeyA2 && nodeKeyB1 === nodeKeyB2) ||
-           (nodeKeyA1 === nodeKeyB2 && nodeKeyB1 === nodeKeyA2)
+  static isEqualNodeKeyPair(nodeKeyA1, nodeKeyB1, nodeKeyA2, nodeKeyB2) {
+    return (
+      (nodeKeyA1 === nodeKeyA2 && nodeKeyB1 === nodeKeyB2) ||
+      (nodeKeyA1 === nodeKeyB2 && nodeKeyB1 === nodeKeyA2)
+    );
   }
   /**
    * Turns an object to an array of objects
    * @param {Object} obj
    * @return {Array.<Object>}
    */
-  static toArray (obj) {
-    let stack = []
+  static toArray(obj) {
+    let stack = [];
     for (let key in obj) {
       if (obj.hasOwnProperty(key)) {
-        stack.push(obj[key])
+        stack.push(obj[key]);
       }
     }
-    return stack
+    return stack;
   }
 }
 
@@ -147,68 +155,76 @@ class GraphAgentAStarSearch extends GraphAgent {
    * @see https://github.com/nervgh/astar-algorithm
    * @param {String} nodeKey
    */
-  expand (nodeKey) {
-    const parentNode = this.problem.nodes[nodeKey]
+  expand(nodeKey) {
+    const parentNode = this.problem.nodes[nodeKey];
 
     if (this.problem.isGoal(parentNode.id)) {
-      return
+      return;
     }
 
-    this.problem.removeFromFrontier(parentNode.id)
-    this.problem.addToExplored(parentNode.id)
+    this.problem.removeFromFrontier(parentNode.id);
+    this.problem.addToExplored(parentNode.id);
 
     for (const successorNode of this.problem.getSuccessors(parentNode.id)) {
       if (this.problem.isExploredNode(successorNode.id)) {
-        continue
+        continue;
       }
 
-      successorNode.depth = parentNode.depth + 1
-      successorNode.parent = parentNode.id
+      successorNode.depth = parentNode.depth + 1;
+      successorNode.parent = parentNode.id;
 
       // The distance from start to a successor
-      const tentativeGScore = parentNode.cost + this.problem.distance(parentNode.id, successorNode.id)
+      const tentativeGScore =
+        parentNode.cost +
+        this.problem.distance(parentNode.id, successorNode.id);
 
       // This is not a better path
       if (tentativeGScore >= successorNode.cost) {
-        continue
+        continue;
       }
 
       // This path is the best until now. We should save it.
-      successorNode.cost = tentativeGScore
-      successorNode.estimatedCost = this.problem.estimate(successorNode.id)
-      successorNode.totalCost = successorNode.cost + successorNode.estimatedCost
+      successorNode.cost = tentativeGScore;
+      successorNode.estimatedCost = this.problem.estimate(successorNode.id);
+      successorNode.totalCost =
+        successorNode.cost + successorNode.estimatedCost;
 
       if (!this.problem.isQueuedNode(successorNode.id)) {
-        this.problem.addToFrontier(successorNode.id)
+        this.problem.addToFrontier(successorNode.id);
       }
     }
 
     // We should prioritize the queue items
     this.problem.frontier.sort((keyA, keyB) => {
-      return this.problem.nodes[keyA].totalCost - this.problem.nodes[keyB].totalCost
-    })
+      return (
+        this.problem.nodes[keyA].totalCost - this.problem.nodes[keyB].totalCost
+      );
+    });
   }
   /**
    * Solves a problem and returns how many iterations it takes
    * @param {Number} [iterationsCount=Number.POSITIVE_INFINITY]
    * @returns {Number}
    */
-  solve (iterationsCount = Number.POSITIVE_INFINITY) {
-    let k = 0
+  solve(iterationsCount = Number.POSITIVE_INFINITY) {
+    let k = 0;
 
-    while (iterationsCount > 0 && !this.problem.isGoal(this.problem.frontier[0])) {
+    while (
+      iterationsCount > 0 &&
+      !this.problem.isGoal(this.problem.frontier[0])
+    ) {
       // Expands next node
-      this.expand(this.problem.frontier[0])
+      this.expand(this.problem.frontier[0]);
 
       // Highlights a node which will be expanded at next iteration
-      const nextIterationNodeKey = this.problem.frontier[0]
-      const nextIterationNode = this.problem.nodes[nextIterationNodeKey]
-      nextIterationNode.state = 'next'
+      const nextIterationNodeKey = this.problem.frontier[0];
+      const nextIterationNode = this.problem.nodes[nextIterationNodeKey];
+      nextIterationNode.state = "next";
 
-      iterationsCount -= 1
-      k += 1
+      iterationsCount -= 1;
+      k += 1;
     }
 
-    return k
+    return k;
   }
 }
